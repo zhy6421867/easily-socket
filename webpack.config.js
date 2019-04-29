66447fd20dbbaa44909bcc74dfbceb149fb5e9eb
zhongyonghao <zhy6421867@qq.com>
2019-04-29 18:27:31 +0800

    update webpack config
    update webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: ['babel-loader']
+    }]
+  }
+}
