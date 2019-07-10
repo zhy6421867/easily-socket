aa3aa59ff357a290cbcc76bb1bd0fb035399cd2d
zhongyonghao <zhy6421867@qq.com>
2019-07-10 10:59:27 +0800

    change index

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import babel from 'rollup-plugin-babel'
 export default {
   input: 'src/main.js',
   output: {
-    file: 'dist/index.js',
+    file: './index.js',
     format: 'cjs',
     name: 'socket'
   },
