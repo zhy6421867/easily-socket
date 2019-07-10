ce3c02515fe50a5a52b381cc51ffc86f87448dbc
zhongyonghao <zhy6421867@qq.com>
2019-07-10 10:39:17 +0800

    add rollup pack command
    add rollup pack command

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import { uglify } from "rollup-plugin-uglify"
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+
+export default {
+  input: 'src/main.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs',
+    name: 'socket'
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    uglify()
+  ]
+}
---
+++ src/main.js
@@ -0,0 +1,108 @@
+class socket {
+
+  // heartBeatTimer = null
+  // options = {}
+  // messageMap = {}
+  // connState = 0
+  // socket = null
+  // url = ''
+  // timeout = 60000
+  // connetCount = 10
+
+  constructor(url = '', options) {
+    this.heartBeatTimer = null
+    this.options = options
+    this.messageMap = {}
+    this.connState = 0
+    this.socket = null
+    this.url = url
+    this.timeout = 60000
+    this.connetCount = 10
+  }
+  doOpen() {
+    if (this.connState) return
+    this.connState = 1
+    this.afterOpenEmit = []
+    const BrowserWebSocket = window.WebSocket || window.MozWebSocket
+    const socket = new BrowserWebSocket(this.url)
+    socket.binaryType = 'arraybuffer'
+    socket.onopen = evt => this.onOpen(evt)
+    socket.onclose = evt => this.onClose(evt)
+    socket.onmessage = evt => this.onMessage(evt.data)
+    socket.onerror = err => this.onError(err)
+    this.socket = socket
+  }
+  onOpen(evt) {
+    this.connState = 2
+    this.heartBeatTimer = setInterval(this.checkHeartbeat.bind(this), 30000)
+    this.onReceiver({ Event: 'open' })
+  }
+  checkOpen() {
+    return this.connState === 2
+  }
+  onClose() {
+    this.connState = 0
+    if (this.connState) {
+      this.onReceiver({ Event: 'close' })
+    }
+    if (this.connetCount > -1) {
+      setTimeout(() => {
+        this.doOpen()
+        this.connetCount--
+      }, 5000)
+    }
+  }
+  send(data) {
+    if (!this.checkOpen()) return
+    this.socket.send(JSON.stringify(data))
+  }
+  emit(data) {
+    return new Promise(resolve => {
+      this.socket.send(JSON.stringify(data))
+      this.on('message', data => {
+        resolve(data)
+      })
+    })
+  }
+  onMessage(message) {
+    try {
+      const data = JSON.parse(message)
+      this.onReceiver({ Event: 'message', Data: data })
+    } catch (err) {
+      console.error(' >> Data parsing error:', err)
+    }
+  }
+  checkHeartbeat() {
+    const data = {
+      'cmd': 'ping',
+      'args': [Date.parse(new Date())]
+    }
+    this.send(data)
+  }
+  onError(err) {
+    console.error(err)
+  }
+  onReceiver(data) {
+    const callback = this.messageMap[data.Event]
+    if (callback) callback(data.Data)
+  }
+  on(name, handler) {
+    this.messageMap[name] = handler
+  }
+  doClose() {
+    this.socket.close()
+  }
+  destroy() {
+    if (this.heartBeatTimer) {
+      clearInterval(this.heartBeatTimer)
+      this.heartBeatTimer = null
+    }
+    this.connetCount = -1
+    this.doClose()
+    this.messageMap = {}
+    this.connState = 0
+    this.socket = null
+  }
+}
+
+export default socket
